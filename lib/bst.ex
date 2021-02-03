c1e551ff2a001c7b888c781a3986817ba9e65c13
Hugo Morgan <hpjm37@gmail.com>
2021-02-03 14:18:25 +0000

    remove boilerplate code; add insert/2

--- lib/bst.ex
+++ lib/bst.ex
@@ -1,18 +1,40 @@
 defmodule BST do
   @moduledoc """
-  Documentation for `BST`.
+  Handles operations for working with binary search trees.
   """
 
+  alias BST.Node
+
   @doc """
-  Hello world.
+  Inserts a node into the tree.
 
   ## Examples
 
-      iex> BST.hello()
-      :world
+      iex> root = BST.Node.new(2)
+      iex> BST.insert(root, 3)
+      %BST.Node{data: 2, right: %BST.Node{data: 3}}
 
   """
-  def hello do
-    :world
+
+  # At leaf - return new node
+  def insert(nil, data) do
+    Node.new(data)
+  end
+
+  # Lower value than current node - recurse down left subtree
+  def insert(%Node{left: left, right: right, data: data}, to_insert)
+      when to_insert < data do
+    Node.new(data, insert(left, to_insert), right)
+  end
+
+  # Greater value than current node - recurse down right subtree
+  def insert(%Node{left: left, right: right, data: data}, to_insert)
+      when to_insert > data do
+    Node.new(data, left, insert(right, to_insert))
+  end
+
+  # Equal - just return node
+  def insert(%Node{left: left, right: right, data: parent_data}, _data) do
+    Node.new(parent_data, left, right)
   end
 end
--- test/bst_test.exs
+++ test/bst_test.exs
@@ -2,7 +2,41 @@ defmodule BSTTest do
   use ExUnit.Case
   doctest BST
 
-  test "greets the world" do
-    assert BST.hello() == :world
+  alias BST.Node
+
+  setup do
+    %{root: Node.new(10)}
+  end
+
+  describe "insert/2" do
+    test "insert/2 inserts on left correctly", %{root: root} do
+      tree = BST.insert(root, 5)
+
+      assert tree.left.data == 5
+    end
+
+    test "insert/2 inserts on right correctly", %{root: root} do
+      tree = BST.insert(root, 11)
+
+      assert tree.right.data == 11
+    end
+
+    test "insert/2 returns tree if nodes equal", %{root: root} do
+      tree = BST.insert(root, 10)
+
+      assert tree == root
+    end
+
+    test "insert/2 works at depth", %{root: root} do
+      tree =
+        root
+        |> BST.insert(5)
+        |> BST.insert(11)
+        |> BST.insert(6)
+        |> BST.insert(15)
+
+      assert tree.left.right.data == 6
+      assert tree.right.right.data == 15
+    end
   end
 end
