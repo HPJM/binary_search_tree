57552b7138aa16ad7d87f2f812c486b0c0186277
Hugo Morgan <hpjm37@gmail.com>
2021-02-03 13:12:56 +0000

    init commit

---
+++ lib/bst.ex
@@ -0,0 +1,18 @@
+defmodule BST do
+  @moduledoc """
+  Documentation for `BST`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BST.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
